15896b73654cc4c7ec4720fcb9821e0f2a586d6d
Assis-S-M <87490535+Assis-S-M@users.noreply.github.com>
2022-06-23 14:01:24 -0300

    Add files via upload

--- docs/js/jquery-validate/dist/localization/messages_pt_BR.js
+++ docs/js/jquery-validate/dist/localization/messages_pt_BR.js
@@ -16,9 +16,9 @@
 $.extend( $.validator.messages, {
 
 	// Core
-	required: "Campo obrigatorio",
+	required: "<img id='imagemAlerta' src='images/alerta.png'></img>",
 	remote: "Por favor, corrija este campo.",
-	email: "Por favor, forne&ccedil;a um endere&ccedil;o de email v&aacute;lido.",
+	email: "Por favor, forne&ccedil;a um email v&aacute;lido.",
 	url: "Por favor, forne&ccedil;a uma URL v&aacute;lida.",
 	date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
 	dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
